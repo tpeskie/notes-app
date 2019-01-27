820376c5b76a38439f4f100bfd0002b8fbde9423
Tom Peskie <29521778+tpeskie@users.noreply.github.com>
2019-01-27 11:46:40 -0800

    Refactor failure response

--- libs/response-lib.js
+++ libs/response-lib.js
@@ -2,13 +2,14 @@ export function success(body) {
   return buildResponse(200, body);
 }
 
-export function failure(error) {
-  return buildResponse(500, {status: false, ...error});
+export function failure(error = {}) {
+  body = {status: false, ...error};
+  return buildResponse(500, body);
 }
 
 function buildResponse(statusCode, body) {
   return {
-    statusCode: statusCode,
+    statusCode,
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true
